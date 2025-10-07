71d73291eb4d0298b8cbfc1323f1ccde96806061
YefanHua <yh3815@columbia.edu>
2025-10-07 19:41:40 -0400

    1

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -4,16 +4,15 @@ def getBondPrice_E(face, couponRate, yc):
     
     pvcf = 0
     
-    for i in range(5):
-        if yc[i] == yc[-1]:
+    for i,j in enumerate(yc):
+        if j == yc[-1]:
             c = face * couponRate + face
-            pv = (1 + yc[i]) ** (-i - 1)
+            pv = (1 + j) ** (-i - 1)
             pvcf = pvcf + c * pv
             
         else:
             c = face * couponRate
-            pv = (1 + yc[i]) ** (-i - 1)
+            pv = (1 + j) ** (-i - 1)
             pvcf = pvcf + c * pv
     
     return(pvcf)
-
--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -4,15 +4,15 @@ def getBondPrice_Z(face, couponRate, times, yc):
         
     pvcf = 0
     
-    for i in range(5):
-        if yc[i] == yc[-1]:
+    for i,j in zip(yc,times):
+        if i == yc[-1]:
             c = face * couponRate + face
-            pv = (1 + yc[i]) ** (-times[i])
+            pv = (1 + i) ** (-j)
             pvcf = pvcf + c * pv
             
         else:
             c = face * couponRate
-            pv = (1 + yc[i]) ** (-times[i])
+            pv = (1 + i) ** (-j)
             pvcf = pvcf + c * pv
     
     return(pvcf)
